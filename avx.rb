26b246c60bbb997c1ce344a9ab20abf304e1218e
Cyrus Javan <javan.cyrus@gmail.com>
2021-02-10 11:18:55 -0800

    update shasum

--- avx.rb
+++ avx.rb
@@ -2,7 +2,7 @@ class Avx < Formula
   desc "A simple CLI for the Aviatrix API."
   homepage "https://github.com/CyrusJavan/avx"
   url "https://github.com/CyrusJavan/avx/archive/v0.0.1-alpha.4.tar.gz"
-  sha256 "409af29b43277bac0e95700ae07e965056ba656b"
+  sha256 "b8e80938323c62ba2b972c92c6810ed9531473a27420aa302f441b47d2c316f6"
 
   depends_on "go"
 
